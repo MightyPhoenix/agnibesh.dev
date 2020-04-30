b4a842685cb05a7798bd5c719491eae871f5e623
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2020-04-30 20:44:51 +0530

    theme-color added

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ const index = () =>{
         <Head>
             <meta charSet="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta name="theme-color" content="#000" />
             <title>Agnibesh.dev</title>
             <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
                     integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
