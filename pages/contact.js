b966e33137c275a418feba2b7e1b90a374623144
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2020-05-06 23:42:59 +0530

    contact page progress

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,10 @@
 import React, {useEffect} from 'react';
-import Head from "next/head";
 import Navbar from "../src/Navbar/Navbar";
 import Footer from "../src/Footer/Footer";
-import {Params} from "../src/ParticleParams";
+import {Params, paramsrainbow} from "../src/ParticleParams";
 import Particles from "react-particles-js";
+import HEAD from "../src/HEAD/HEAD";
+
 if (typeof window !== 'undefined'){
     require('materialize-css');
 }
@@ -17,31 +18,37 @@ const Contact = () => {
     },[]);
     return (
         <div className="body">
-            <Head>
-                <meta charSet="UTF-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <meta name="theme-color" content="#000" />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/>
-                <title>Agnibesh.dev</title>
-                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-                        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-                        crossOrigin="anonymous"/>
-                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-                        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-                        crossOrigin="anonymous"/>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-                        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-                        crossOrigin="anonymous"/>
-            </Head>
+            <HEAD/>
             {/*HEAD END*/}
             <Navbar NavItems={Nav}/>
             {/*NAV END*/}
-            <main className="container center valign-wrapper">
+            <main className="container center valign-wrapper" style={{zIndex:"1000"}}>
+                <div className={"section row"}>
 
+                    <div className={"col s12 m4"} style={{marginBottom:"10px",marginTop:"10px"}}>
+                        <div className={"white"} style={{magin:"0px",padding:"10px", borderRadius:"8px"}}>
+                        <h4 className={"font-neucha"}>Basically my digital footprint</h4>
+                        </div>
+                    </div>
+
+                    <div className={"col s12 m4"} style={{marginBottom:"10px",marginTop:"10px"}}>
+                        <div className={"pinkgrad"} style={{magin:"0px",padding:"15px 15px 15px 15px", borderRadius:"8px"}}>
+                        <h3 className={"font-caveat white-text"}>Trace me here...</h3>
+                        <p className={"red-text font-vt"} style={{fontSize:"18px"}}>Dont try to act smart to use my API key, it has restrictions!</p>
+                        <img alt={"loc"} className={"responsive-img"} style={{borderRadius:"8px"}} src={"https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDdZ2uRKuXlYmpj_OhhPODhalh5ccTsOIg&center=22.67639858740434,88.32972875000003&zoom=10&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x1d2c4d&style=element:labels.text.fill%7Ccolor:0x8ec3b9&style=element:labels.text.stroke%7Ccolor:0x1a3646&style=feature:administrative%7Celement:geometry%7Cvisibility:off&style=feature:administrative.country%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0x64779e&style=feature:administrative.province%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:landscape.man_made%7Celement:geometry.stroke%7Ccolor:0x334e87&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0x023e58&style=feature:poi%7Cvisibility:off&style=feature:poi%7Celement:geometry%7Ccolor:0x283d6a&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x6f9ba5&style=feature:poi%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0x023e58&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x3C7680&style=feature:road%7Celement:geometry%7Ccolor:0x304a7d&style=feature:road%7Celement:labels.icon%7Cvisibility:off&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:road%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:road.highway%7Celement:geometry%7Ccolor:0x2c6675&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0x255763&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0xb0d5ce&style=feature:road.highway%7Celement:labels.text.stroke%7Ccolor:0x023e58&style=feature:transit%7Cvisibility:off&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:transit%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:transit.line%7Celement:geometry.fill%7Ccolor:0x283d6a&style=feature:transit.station%7Celement:geometry%7Ccolor:0x3a4762&style=feature:water%7Celement:geometry%7Ccolor:0x0e1626&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x4e6d70&size=480x360"}/>
+                        </div>
+                    </div>
+
+                    <div className={"col s12 m4"} style={{marginBottom:"10px",marginTop:"10px"}}>
+                        <div className={"white"} style={{magin:"0px",padding:"15px 15px 15px 15px", borderRadius:"8px"}}>
+                        <h3 className={"font-neucha"}>Basically my digital footprint</h3>
+                        </div>
+                    </div>
+
+                </div>
             </main>
             <Footer/>
-            <Particles params={Params} id="particles"/>
+            <Particles params={paramsrainbow} id="particles"/>
         </div>
     );
 };
