581dfc29f87c91cf9d91378356d7d4272627e327
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2020-05-10 20:56:39 +0530

    fixes

--- pages/contact.js
+++ pages/contact.js
@@ -6,6 +6,7 @@ import {IconContext} from 'react-icons';
 import {BsFillEnvelopeFill} from 'react-icons/bs';
 import {FaGithub, FaLinkedin} from 'react-icons/fa';
 import {AiOutlineInstagram} from 'react-icons/ai';
+import {GiFootprint} from 'react-icons/gi';
 
 import Particles from "react-particles-js";
 import HEAD from "../src/HEAD/HEAD";
@@ -32,28 +33,30 @@ const Contact = () => {
 
                     <div className={"col s12 m4"} style={{marginBottom:"10px",marginTop:"10px"}}>
                         <div className={"white"} style={{magin:"0px",padding:"10px", borderRadius:"8px",backgroundImage: "linear-gradient(60deg, #3d3393 0%, #2b76b9 37%, #2cacd1 65%, #35eb93 100%)"}}>
-                        <h4 className={"font-neucha white-text"}>Basically my digital footprint</h4>
+                            <IconContext.Provider  value={{size:"2rem",color:"white",style:{margin:"0 8px"}}}>
+                        <h3 className={"font-neucha white-text"}>Basically my digital footprint<GiFootprint/></h3>
+                            </IconContext.Provider>
                             <IconContext.Provider  value={{size:"0.5rem",color:"white",}}>
-                            <ul className="collection white-text font-neucha left-align flow-text" style={{border:"0px solid white",background:"rgba(0,0,0,0)"}}>
+                            <ul className="collection white-text font-neucha left-align" style={{border:"0px solid white",background:"rgba(0,0,0,0)",fontSize:"1.5rem"}}>
 
                                 <li className="collection-item avatar" style={{border:"0px solid white",background:"rgba(0,0,0,0)"}}>
                                     <BsFillEnvelopeFill className={"circle"} style={{borderRadius:"0"}}/>
-                                    <p className={"flow-text"} style={{marginTop:"4%"}}><a href={"mailto:agnibesh00mukherjee@gmail.com"} className={"white-text"}>Email me Old-School</a></p>
+                                    <p style={{marginTop:"5%"}}><a href={"mailto:agnibesh00mukherjee@gmail.com"} className={"white-text"}>Email me Old-School</a></p>
                                 </li>
 
                                 <li className="collection-item avatar" style={{border:"0px solid white",background:"rgba(0,0,0,0)"}}>
                                     <FaLinkedin className={"circle"} style={{borderRadius:"0"}}/>
-                                    <p style={{marginTop:"4%"}}><a className={"white-text"} href={"https://www.linkedin.com/in/mightyphoenix7/"}>Industry norm</a></p>
+                                    <p style={{marginTop:"5%"}}><a className={"white-text"} href={"https://www.linkedin.com/in/mightyphoenix7/"}>Industry norm</a></p>
                                 </li>
 
                                 <li className="collection-item avatar" style={{border:"0px solid white",background:"rgba(0,0,0,0)"}}>
                                     <FaGithub className={"circle"} style={{borderRadius:"0"}}/>
-                                    <p style={{marginTop:"4%"}}><a className={"white-text"} href={"https://www.github.com/MightyPhoenix"}>Github for all them codes</a></p>
+                                    <p style={{marginTop:"5%"}}><a className={"white-text"} href={"https://www.github.com/MightyPhoenix"}>Github for all them codes</a></p>
                                 </li>
 
                                 <li className="collection-item avatar" style={{border:"0px solid white",background:"rgba(0,0,0,0)"}}>
                                     <AiOutlineInstagram className={"circle"} style={{borderRadius:"0"}}/>
-                                    <p style={{marginTop:"4%"}}><a className={"white-text"} href={"https://www.instagram.com/hsebinga"}>The Gram'</a></p>
+                                    <p style={{marginTop:"5%"}}><a className={"white-text"} href={"https://www.instagram.com/hsebinga"}>The Gram'</a></p>
                                 </li>
 
                             </ul>
@@ -66,7 +69,7 @@ const Contact = () => {
                         <h3 className={"font-caveat white-text"}>Trace me here...</h3>
                             <div className="mapouter center-align" style={{margin:"auto"}}>
                                 <div className="gmap_canvas">
-                                    <iframe width="320" height="320" id="gmap_canvas" src="https://maps.google.com/maps?q=Kolkata&t=&z=11&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
+                                    <iframe style={{borderRadius:"8px"}} width="320" height="320" id="gmap_canvas" src="https://maps.google.com/maps?q=Kolkata&t=&z=11&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
                                 </div>
                                     <style>{`
                                 .mapouter{
